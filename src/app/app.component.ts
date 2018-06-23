51b5c8bdcad89c0d80d5182f310fd197bd779da3
harshitbhargava <120597+harshitbhargava@users.noreply.github.com>
2018-06-23 23:53:37 +0530

    gender

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name;
+  gender : any;
+  url= 'https://api.genderize.io/?name='
+
+constructor(private http : HttpClient){ }
+getdata(){
+
+this.http.get(this.url+this.name).subscribe((data : any)=>{
+  this.gender=data.gender;
+  console.log(data);
+})
+}
 }
